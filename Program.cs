d112548b9d13560b19c4a0717ceffc8d571221d2
Courtney Gulledge <courtney.gulledge@gmail.com>
2023-06-07 10:36:30 -0400

    initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GroupHeist
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
